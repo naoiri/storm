40cadcb1a0bd6736b270fa8a07078630f18213a3
Marten Hernebring <marten.hernebring@gmail.com>
2021-03-16 15:53:50 +0100

    Made a simple Welcome app

--- search/src/App.vue
+++ search/src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <hello-world msg="Välkommen" />
 </template>
 
 <script>
@@ -17,8 +16,6 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
