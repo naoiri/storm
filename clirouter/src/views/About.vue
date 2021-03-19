4be211ecf94040abd414fd7d51cac26fc8ce7f8a
Marten Hernebring <marten.hernebring@gmail.com>
2021-03-19 13:14:03 +0100

    Minor formatting of files

--- clirouter/src/views/About.vue
+++ clirouter/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="about">
-        <h1>Version 1.2</h1>
+        <h1>Version 1.3</h1>
         <p>API:https://opendata.smhi.se/apidocs/warnings/index.html</p>
         <p>Email: marten.hernebring@gmail.com</p>
     </div>
