122039215333ee13379cf719b7ec19b3499713b3
Marten Hernebring <marten.hernebring@gmail.com>
2021-03-22 20:22:30 +0100

    Added original temperature code to vue

--- clirouter/src/views/About.vue
+++ clirouter/src/views/About.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="about">
         <Title msg="Om oss i Storm" />
-        <p><strong>About us who created this app: we are UX and Java students in Yrgo Gothenburg Sweden.</strong> </p>
+        <p><strong>About us who created this app: we are UX and Java students in Yrgo Gothenburg Sweden.</strong></p>
         <p>Version 1.0.5"</p>
-        <p>API:https://opendata.smhi.se/apidocs/warnings/index.html</p>
+        <p>API1:https://opendata.smhi.se/apidocs/warnings/index.html</p>
+        <p>API2A: https://opendata.smhi.se/apidocs/metfcst/get-forecast.html</p>
+        <p>API2B: https://opendata.smhi.se/apidocs/metfcst/parameters.html</p>
         <p>Email: marten.hernebring@gmail.com</p>
     </div>
 </template>
