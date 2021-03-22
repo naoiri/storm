8d3fec262933149f72cd2a390ed77088d2ceb4a8
Marten Hernebring <marten.hernebring@gmail.com>
2021-03-22 11:42:58 +0100

    Changed prettier to ignore new lines and added Intro component in home

---
+++ clirouter/src/components/Intro.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="intro">
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Intro",
+    props: {
+        msg: String,
+    },
+}
+</script>
--- clirouter/src/views/Home.vue
+++ clirouter/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="home">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <HelloWorld msg="Välkommen" />
+        <Intro msg="hej hej här är lite väder" />
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue"
+import Intro from "@/components/Intro.vue"
 
 export default {
     name: "Home",
     components: {
         HelloWorld,
+        Intro,
     },
 }
 </script>
