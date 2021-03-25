7a8afa30c05c606b6a602f64fdd1fed4502015a8
Marten Hernebring <marten.hernebring@gmail.com>
2021-03-25 18:01:53 +0100

    Added data 4 months back 7 biggest cities

--- clirouter/src/views/Compare.vue
+++ clirouter/src/views/Compare.vue
@@ -21,7 +21,7 @@
             </span>
         </div-->
         <div class="data main center">
-            <div>Vädret i {{ query }} för 4 månader sedan var: {{ temperatureData }} grader Celcius.</div>
+            <div>Vädret i {{ current }} för 4 månader sedan var: {{ temperatureData }} grader Celcius.</div>
         </div>
     </div>
 </template>
@@ -40,24 +40,44 @@ export default {
     data() {
         return {
             cities: [],
+            current: "",
             query: "",
             parameter1: [],
             temperatureData: "",
         }
     },
     methods: {
-        async updateData() {
-            //GET request using fetch with async/await
-            if (this.query === "Stockholm") {
-                const response = await fetch(
-                    "https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station/98230/period/latest-months/data.json"
-                )
-                const json = await response.json()
-                this.parameter1 = json.value
-                this.temperatureData = this.parameter1[0].value
-            } else {
-                this.temperatureData = ""
+        getCityId() {
+            switch (this.query) {
+                case "Stockholm":
+                    return "98230"
+                case "Göteborg":
+                    return "71420"
+                case "Malmö":
+                    return "52350"
+                case "Uppsala":
+                    return "97510"
+                case "Västerås":
+                    return "96350"
+                case "Örebro":
+                    return "94190"
+                case "Helsingborg":
+                    return "62040"
+                default:
+                    return "00000"
             }
+        },
+        async updateData() {
+            const response = await fetch(
+                "https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station/" +
+                    this.getCityId() +
+                    "/period/latest-months/data.json"
+            )
+            console.log(response)
+            const json = await response.json()
+            this.parameter1 = json.value
+            this.temperatureData = this.parameter1[0].value
+            this.current = this.query
         },
     },
     created() {
