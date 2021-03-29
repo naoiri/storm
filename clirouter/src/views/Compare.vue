fc0904410f54619e2f5f95474f14f280f87d8365
Marten Hernebring <marten.hernebring@gmail.com>
2021-03-29 19:12:27 +0200

    Present temperature is also showned

--- clirouter/src/views/Compare.vue
+++ clirouter/src/views/Compare.vue
@@ -71,7 +71,7 @@ export default {
         diff_hours(dt2, dt1) {
             var diff = (dt2.getTime() - dt1.getTime()) / 1000
             diff /= 60 * 60
-            if (diff !== 0) {
+            if (diff > 12) {
                 diff -= 12
             }
             return Math.abs(Math.round(diff))
