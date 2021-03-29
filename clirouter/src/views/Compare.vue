4618c2e5141b99106abcab42ec9fa00b00f090f5
Marten Hernebring <marten.hernebring@gmail.com>
2021-03-29 19:09:48 +0200

    Changed temperature from night to day

--- clirouter/src/views/Compare.vue
+++ clirouter/src/views/Compare.vue
@@ -71,6 +71,9 @@ export default {
         diff_hours(dt2, dt1) {
             var diff = (dt2.getTime() - dt1.getTime()) / 1000
             diff /= 60 * 60
+            if (diff !== 0) {
+                diff -= 12
+            }
             return Math.abs(Math.round(diff))
         },
     },
