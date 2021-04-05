3676bc9253e0d2557998ffa8cd2da0f8d59fca2d
Marten Hernebring <marten.hernebring@gmail.com>
2021-04-05 11:22:11 +0200

    Removed ort 1 text

--- clirouter/src/views/Compare.vue
+++ clirouter/src/views/Compare.vue
@@ -4,7 +4,6 @@
         <p class="strong">Se vilken temperatur det var historiskt t.o.m. December 2020.</p>
         <DatePicker id="datePicker" v-model="date" />
         <div class="main">
-            <span class="text center">Ort 1: </span>
             <span class="autoComplete_wrapper">
                 <input id="autoComplete" type="text" autocomplete="off" />
             </span>
