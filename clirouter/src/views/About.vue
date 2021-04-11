75168f5c53a2040f5c0442dd6b760f9e838c3ed1
suzyrgo <suztra0803@skola.goteborg.se>
2021-04-11 22:39:21 +0200

    changed the text and layout in about.vue

--- clirouter/src/views/About.vue
+++ clirouter/src/views/About.vue
@@ -15,31 +15,33 @@
             <p>API3: https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station/</p>
         </div>
         <div class="contact">
-            <p>Mårten, Java, Email: <br />marten.hernebring@gmail.com</p>
-            <p>Naoya, Java, Email: naoya.irikura@skola.goteborg.se</p>
-            <p>Suzana, Java, Email: <br />suztra0803@skola.goteborg.se</p>
+            <p><strong>Mårten, Java</strong>, Email: <br />marten.hernebring@gmail.com</p>
+            <p><strong>Naoya, Java</strong>, Email: <br />naoya.irikura@skola.goteborg.se</p>
+            <p><strong>Suzana, Java</strong>, Email: <br />suztra0803@skola.goteborg.se</p>
             <p>
-                Jon, studerar Ux-design på Yrgo, har pluggat systemvetenskap och psykologi sedan innan. Största
-                intressen är matlagning och musik. När Sverige inte bjuder på permafrost gillar Jon att vara ute i
-                naturen, paddla kajak och vandra. Email: jonkarlstrom@hotmail.com
+                <strong>Jon, studerar Ux-design på Yrgo</strong>, har pluggat systemvetenskap och psykologi sedan innan.
+                Största intressen är matlagning och musik. När Sverige inte bjuder på permafrost gillar Jon att vara ute
+                i naturen, paddla kajak och vandra.<br />
+                Email: jonkarlstrom@hotmail.com
             </p>
             <p>
-                Sandra, studerar UX-design på Yrgo. Har de senaste 6 åren jobbat inom produktion och främst i projekt
-                för att utveckla produktion av nya produkter, så som bioreaktorer och litsiumjonbatterier. Sandra åker
-                helst på långa resor till varmare breddgrader med fina vågor för surfing eller höga berg att klättra på.
-                Har även en teknisk gymnasieutbildning och en salig blandning av universitetskurser i bagaget. Email:
-                sandra.125@live.se
+                <strong>Sandra, studerar UX-design på Yrgo</strong>. Har de senaste 6 åren jobbat inom produktion och
+                främst i projekt för att utveckla produktion av nya produkter, så som bioreaktorer och
+                litsiumjonbatterier. Sandra åker helst på långa resor till varmare breddgrader med fina vågor för
+                surfing eller höga berg att klättra på. Har även en teknisk gymnasieutbildning och en salig blandning av
+                universitetskurser i bagaget. <br />Email: sandra.125@live.se
             </p>
             <p>
-                Noa, studerar UX-design just nu och har hållit på med lite olika grejer innan. Har jobbat på
-                landvetterflygplats och lite lätt som föreskolevikarie. Har sedan tidigare en Tekniskt gymnasie
-                utbildning på Hulebäck. Email: noalillskog@gmail.com
+                <strong>Noa, studerar UX-design</strong> just nu och har hållit på med lite olika grejer innan. Har
+                jobbat på landvetterflygplats och lite lätt som föreskolevikarie. Har sedan tidigare en Tekniskt
+                gymnasie utbildning på Hulebäck. <br />Email: noalillskog@gmail.com
             </p>
             <p>
-                Jenny Studerar Ux-design och har sedan innan en socialantropologisk utbildning i bagaget samt kunskaper
-                inom områden som media- och kommunikation, verksamhetsplanering, hälsosamordning och administration.
-                Jenny har även en konstnärlig ådra och en förkärlek för att uttrycka den genom blandteknik på stora
-                skivor av plywood, måla möbler eller smida i silver Email: jy.nilsson@outlook.com
+                <strong>Jenny Studerar Ux-design</strong> och har sedan innan en socialantropologisk utbildning i
+                bagaget samt kunskaper inom områden som media- och kommunikation, verksamhetsplanering, hälsosamordning
+                och administration. Jenny har även en konstnärlig ådra och en förkärlek för att uttrycka den genom
+                blandteknik på stora skivor av plywood, måla möbler eller smida i silver. <br />Email:
+                jy.nilsson@outlook.com
             </p>
         </div>
     </div>
