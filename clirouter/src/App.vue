27929a5ca3a0d67c7e122e6b432df029a8f8567a
Naoya Irikura <naoiri2001@skola.goteborg.se>
2021-04-12 18:51:13 +0200

    Changed the font-size and height in medium screen

--- clirouter/src/App.vue
+++ clirouter/src/App.vue
@@ -17,9 +17,9 @@
         </div>
     </nav>
     <nav class="big-screen">
-        <router-link to="/">Start</router-link> | <router-link to="/about">Om oss</router-link> |
-        <router-link to="/fire">Brandrisk</router-link> | <router-link to="/ski">Skidväder</router-link> |
-        <router-link to="/compare">Ortjämförelse</router-link>
+        <router-link to="/" class="bs-menu">Start</router-link> | <router-link to="/about" class="bs-menu">Om oss</router-link> |
+        <router-link to="/fire" class="bs-menu">Brandrisk</router-link> | <router-link to="/ski" class="bs-menu">Skidväder</router-link> |
+        <router-link to="/compare" class="bs-menu">Ortjämförelse</router-link>
     </nav>
     <div class="showcase">
         <div class="container showcase-inner">
@@ -46,12 +46,17 @@ export default {
 }
 .big-screen {
     display: none;
+    height: 100px;
 }
 .about,
 .main,
 .fire-main,
 .maxwidth {
     width: 350px;
+}
+
+.bs-menu {
+    font-size: xx-large;
 }
 
 /* medium, ipad etc */
