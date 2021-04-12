b1fb88e6145d1106ef9afe70c25c6dba5559ab32
suzyrgo <suztra0803@skola.goteborg.se>
2021-04-12 13:09:40 +0200

    changed datepicker

--- clirouter/src/views/Compare.vue
+++ clirouter/src/views/Compare.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="compare">
         <div class="paddingright">
-            <Title msg="Jämför orter" class="main" />
-            <p class="strong main">Jämför temperaturen mellan två orter t.o.m. December 2020.</p>
             <div class="redborder main">
                 <DatePicker class="data blue" id="datePicker" v-model="date" />
             </div>
+            <Title msg="Jämför orter" class="main" />
+            <p class="strong main">Jämför temperaturen mellan två orter t.o.m. December 2020.</p>
             <div class="updownpad main">
                 <div class="text center">Fyll i för {{ whichCity }}. Ort 1: {{ current }}, Ort 2: {{ current2 }}</div>
                 <div class="autoComplete_wrapper">
